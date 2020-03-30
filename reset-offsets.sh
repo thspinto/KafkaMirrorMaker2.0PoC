7bd9210d963ea7246aabd01ad61d6982c9247a50
Thiago Pinto <thiago.pinto@quintoandar.com.br>
2020-03-30 19:45:52 -0300

    added reset script

---
+++ reset-offsets.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+BOOTSTRAP_SERVER=kafka-prod-0.quintoandar.com.br:9092
+
+function reset_offsets() {
+  local GROUP=$1
+  local TOPIC=$2
+  kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVER --group $GROUP --topic $TOPIC --reset-offsets --to-latest --execute
+}
+
+function main () {
+  CGROUPS="$(kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVER --list)"
+  IFS=$'\n'
+  for GROUP in $CGROUPS
+  do
+    echo "Iteration on $GROUP"
+    TOPICS="$(kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVER --describe --group $GROUP | cut -d' ' -f1 | tail -n +3 | sort | uniq)"
+    for TOPIC in $TOPICS
+    do
+      echo "Reseting $TOPIC"
+      reset_offsets $GROUP $TOPIC
+    done
+  done
+}
+
+main
